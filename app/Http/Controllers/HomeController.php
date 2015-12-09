9d231c3a100852ea4be0dca4aee8883314dc8219
snooker <jirayut.kha@awecent.com>
2015-12-09 20:31:44 +0700

    update Home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -51,17 +51,7 @@ class HomeController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
+   
     /**
      * Update the specified resource in storage.
      *
