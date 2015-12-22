a3a218089aef7884a7e83b2582e1a844a6e89eb7
snooker <jirayut.kha@awecent.com>
2015-12-22 16:18:41 +0700

    change seeder user

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -13,17 +13,17 @@ class UserTableSeeder extends Seeder
     public function run()
     {
     	//$datetime = Carbon::now('Asia/Bangkok');
-    	$date = new DateTime();
-    	$date->setTimezone(new DateTimeZone('Asia/Bangkok'));
+        $date = new DateTime();
+        $date->setTimezone(new DateTimeZone('Asia/Bangkok'));
 
         DB::table('users')->insert([
-            'name' => 'Snooker Koleng',
-            'firstName' => 'Jirayut',
-            'lastName' => 'Khantavee',
-            'cardID' => '1350100240034',
-            'phone' => '0874652849',
+            'name' => 'Pathma In',
+            'firstName' => 'Pathma',
+            'lastName' => 'Inpromma',
+            'cardID' => '1490300066761',
+            'phone' => '0909359085',
             'code' => $date->format('YmdHis'),
-            'email' => 'ker.koleng@hotmail.co.th',
+            'email' => 'mychappa@gmail.com',
             'password' => bcrypt('kantavee'),
             'created_at' => $date->format('Y-m-d H:i:s'),
             'role' => 99
