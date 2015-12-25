e41e83afd806df56ea314d8431fcdf92457b0bf8
snooker <jirayut.kha@awecent.com>
2015-12-25 14:39:23 +0700

    fix green box

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -120,20 +120,7 @@
             </div>
         </div>
         @yield('description')
-        <div id="green">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-5 centered">
-                        <img src="/img/iphone.png" alt="">
-                    </div>
-                    
-                    <div class="col-lg-7 centered">
-                        <h3>How to go the Green Vintage?</h3>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        
 
         <div id="footer">
                 <div class="container">
