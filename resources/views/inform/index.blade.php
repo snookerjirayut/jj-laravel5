38acf6c1ce3013d1eb2c3dfef7c875d667bbb2be
Snooker-Jirayut <jirayut.kha@awecent.com>
2016-02-28 20:51:51 +0700

    [*] upload slip - can click able

--- resources/views/inform/index.blade.php
+++ resources/views/inform/index.blade.php
@@ -2,122 +2,133 @@
 @section('title', 'Inform')
 
 @section('style')
-<link href="/css/dropzone.css" rel="stylesheet">
+    <link href="/css/dropzone.css" rel="stylesheet">
 @endsection
 
 @section('breadcrumbs')
-	<div class="breadcrumbs">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-5 col-sm-5">
-					<h1>แจ้งโอนโดยหลักฐานการชำระเงิน</h1>
-				</div>
-				<div class="col-lg-7 col-sm-7">
-					<ol class="breadcrumb pull-right">
-						<li><a href="{{ url('/') }}">หน้าหลัก</a></li>
-						<li class="active">แจ้งโอน</li>
-					</ol>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="breadcrumbs">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-sm-5">
+                    <h1>แจ้งโอนโดยหลักฐานการชำระเงิน</h1>
+                </div>
+                <div class="col-lg-7 col-sm-7">
+                    <ol class="breadcrumb pull-right">
+                        <li><a href="{{ url('/') }}">หน้าหลัก</a></li>
+                        <li class="active">แจ้งโอน</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('content')
-<script src="/js/dropzone.js" ></script>
-{{-- <script src="/js/angular/angular-dropzone.js" ></script> --}}
+    <script src="/js/dropzone.js"></script>
 
+    <style>
+        #dropzone {
+            position: absolute;
+            width: 100%;
+            height:100%;
+            top:0;
+            left: 0;
+        }
+    </style>
 
-<div class="col-sm-6 col-sm-offset-3" ng-controller="UploadController" ng-init="init()">
-	<div class="controls col-lg-12 col-sm-12 form-group has-success box-inform-date">
-		<p class="text-center">วันที่จอง</p>
-		<select ng-model="input.code" ng-options="booking.code as booking.miliseconds | date:'EEEE dd MMMM y' for booking in list.booking" class="form-control box-select-date"></select>
-	</div>
-	<form id="dropzone" name="upload" class="dropzone col-sm-12" action="/inform/upload" method="POST" enctype="multipart/form-data"
-		>
-		<div class="box-inform-upload">
-			
-			<img src="/img/icon-upload.png" class="img-responsive">
+    <div class="col-sm-6 col-sm-offset-3" ng-controller="UploadController" ng-init="init()">
+        <div class="controls col-lg-12 col-sm-12 form-group has-success box-inform-date">
+            <p class="text-center">วันที่จอง</p>
+            <select ng-model="input.code"
+                    ng-options="booking.code as booking.miliseconds | date:'EEEE dd MMMM y' for booking in list.booking"
+                    class="form-control box-select-date"></select>
+        </div>
 
-		</div>
-		<div class="dz-default dz-message"></div>
-	</form>
-	<button class="btn btn-block btn-info btn-upload" type="button" ng-click="save()">อัพโหลด</button>
-<br>
-	{{--  <a class="btn btn-primary" href="" ng-href="<% filename %>"><% filename %></a> --}}
-</div>
+        <div class="box-inform-upload">
+            <img src="/img/icon-upload.png" class="img-responsive">
+            <form id="dropzone" name="upload" class="dropzone col-sm-12"
+                  action="/inform/upload" method="POST" enctype="multipart/form-data">
+
+                <div class="dz-default dz-message"></div>
+            </form>
+        </div>
+
+        <button class="btn btn-block btn-info btn-upload" type="button" ng-click="save()">อัพโหลด</button>
+        <br>
+        {{--  <a class="btn btn-primary" href="" ng-href="<% filename %>"><% filename %></a> --}}
+    </div>
 
 @endsection
 @section('script')
 
-<script type="text/javascript">
-	$('#nav-bar li').eq(2).addClass('active');
+    <script type="text/javascript">
+        $('#nav-bar li').eq(2).addClass('active');
 
-	angular.module("myApp").controller('UploadController', ['$scope' ,'$http' , function($scope , $http){ 
-		
-		var myDropzone = new Dropzone("form#dropzone", { url: "/inform/upload" ,  clickable: false});
-		$scope.input = {};
-		$scope.list = {};
-		$scope.list.booking = [];
+        angular.module("myApp").controller('UploadController', ['$scope', '$http', function ($scope, $http) {
 
-		$scope.file = {};
-		$scope.input.file = {};
-		$scope.fileAdded = false;
+            var myDropzone = new Dropzone("form#dropzone");
+            $scope.input = {};
+            $scope.list = {};
+            $scope.list.booking = [];
 
-		//miliseconds
-		$scope.init = function(){
-			$http.get('/inform/feed').success(function(d){
-				console.log(d);
-				if(d.data.length === 0 ) $scope.list.booking = [];
-				$scope.list.booking = d.data;
-			});
-		}
-		$scope.save = function(){
-			if(Object.keys($scope.input.file).length == 0) return;
-			$http.put('/inform/update/'+$scope.input.code, $scope.input).success(function(data){
-				console.log(data);
-				if(data.result){
-					alert('อัพโหลดเสร็จเรียบร้อย');
-					window.location = '/inform';
-				}else{
-					alert(data.messge);
-				}
-			});
-		}
+            $scope.file = {};
+            $scope.input.file = {};
+            $scope.fileAdded = false;
 
-		$(document).ready(function(){
-			myDropzone.on("addedfile", function(file) {
-			    $scope.file = file;
-                if (this.files[1]!=null) {
-                	this.removeFile(this.files[0]);
-                }
-                $scope.$apply(function() {
-                    $scope.fileAdded = true;
+            //miliseconds
+            $scope.init = function () {
+                $http.get('/inform/feed').success(function (d) {
+                    console.log(d);
+                    if (d.data.length === 0) $scope.list.booking = [];
+                    $scope.list.booking = d.data;
                 });
-			});
+            }
+            $scope.save = function () {
+                if (Object.keys($scope.input.file).length == 0) return;
+                $http.put('/inform/update/' + $scope.input.code, $scope.input).success(function (data) {
+                    console.log(data);
+                    if (data.result) {
+                        alert('อัพโหลดเสร็จเรียบร้อย');
+                        window.location = '/inform';
+                    } else {
+                        alert(data.messge);
+                    }
+                });
+            }
 
-			myDropzone.on('sending', function(file, xhr, formData){
-	            formData.append('filename', $scope.input.code );
-	        });
+            $(document).ready(function () {
+                myDropzone.on("addedfile", function (file) {
+                    $scope.file = file;
+                    if (this.files[1] != null) {
+                        this.removeFile(this.files[0]);
+                    }
+                    $scope.$apply(function () {
+                        $scope.fileAdded = true;
+                    });
+                });
 
-			myDropzone.on("success", function(data) {
-			    var val = JSON.parse(data.xhr.response);
-			    if(val.result){
-			    	console.log(val);
-				  	$scope.input.file = val.filename;
-				  	$scope.$apply();
-			    }else{
-			    	alert(val.message);
-			    	$('div.dz-success').remove();
-			    }
-			    
-			});
+                myDropzone.on('sending', function (file, xhr, formData) {
+                    formData.append('filename', $scope.input.code);
+                });
 
-		});
+                myDropzone.on("success", function (data) {
+                    var val = JSON.parse(data.xhr.response);
+                    if (val.result) {
+                        console.log(val);
+                        $scope.input.file = val.filename;
+                        $scope.$apply();
+                    } else {
+                        alert(val.message);
+                        $('div.dz-success').remove();
+                    }
 
-	}]);
+                });
+
+            });
+
+        }]);
 
 
-</script>
+    </script>
 
 @endsection
